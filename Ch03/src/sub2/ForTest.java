b15fa3ab629056033f12ce3e53f0f22a701a8042
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-05-18 11:17:20 +0900

    2021/05/18

---
+++ Ch03/src/sub2/ForTest.java
@@ -0,0 +1,81 @@
+package sub2;
+/*
+ * 날짜 : 2021/05/18
+ * 이름 :김철학
+ * 내용 : 자바 for 반복문 실습하기
+ */
+public class ForTest {
+	public static void main(String[] args) {
+		
+		//for (초기식, 조건식, 증감식)
+		for(int i=1 ; i<=3 ; i++) {
+			System.out.printf("%d.Hello Java! \n", i);
+				//printf은 서식을 포함한 식
+		}
+		
+		
+		//1부터 10까지 합
+		int sum = 0;
+		
+		for(int k=1 ; k<=10 ; k++) {
+			sum += k;
+		}
+		System.out.println("1부터 10까지의 합 : "+sum);
+		
+		
+		//1부터 10까지 짝수합
+		int esum = 0;
+		
+		for(int k=1 ; k<=10 ; k++) {
+			if(k % 2 == 0) {
+				esum += k;
+			}
+		}
+		System.out.println("1부터 10까지의 합 : "+esum);
+		
+		
+		//중첩 for
+		for(int a=1 ; a<=3 ; a++) {
+			
+			System.out.println("a : "+a);
+			for(int b=1 ; b<=5 ; b++) {
+				System.out.println("b : "+b);
+			}
+		}
+		
+		
+		//구구단
+		for(int x=2 ; x<=9 ; x++) {
+			
+			for(int y=1 ; y<=9 ; y++) {
+				int z = x * y;
+				System.out.printf("%d x %d =%d\n", x, y, z);
+			}
+		}
+		
+		
+		//별삼각형1
+		for(int start=1; start<=10; start++) {
+			
+			for (int end=1 ; end <= start ; end++) {
+				
+				System.out.print("☆");
+			}
+			System.out.print("\n");
+		}
+		
+		
+		//별삼각형2
+		for(int a=1 ; a<=5 ; a++) {
+			
+			for(int b=1; b<=5-a; b++) {
+				System.out.print("☆");
+			}
+			for(int c=1; c<=a; c++) {
+				System.out.print("★");
+			}
+			System.out.print("\n");
+		}
+		
+	}
+}
---
+++ Ch03/src/sub2/WhileTest.java
@@ -0,0 +1,60 @@
+package sub2;
+/*
+ * 날짜 : 2021/05/18
+ * 이름 :김철학
+ * 내용 : 자바 While 반복문 실습하기
+ */
+public class WhileTest {
+	public static void main(String[] args) {
+		
+		//1부터 10까지 합
+		int k = 1;
+		int sum = 0;
+		
+		while(k <= 10) {
+			sum += k;
+			k++;
+		}
+		System.out.println("1부터 10까지 합 : "+sum);
+		
+		//do ~ while
+		int esum= 0;
+		int i = 1;
+		
+		do {
+			//최초 1번은 실행하는 반복문
+			if(i % 2 == 0) {
+			esum += i;
+			}
+			i++;
+		}
+		while(i <= 10);
+		System.out.println("1부터 10까지 짝수합 : "+esum);
+		
+		// break
+		int num = 1;
+		while(true) {
+			
+			num++;
+			
+			if(num % 5 == 0 && num % 7 == 0) {
+				break;
+			}
+		}
+		System.out.println("5와 7의 최소공배수 : "+num);
+		
+		
+		//continue
+		int total = 0;
+		for (int j=1 ; j<=10 ; j++) {
+			if(j % 2 == 1) {
+				continue;
+			}
+			
+			total += j;
+		}
+		System.out.println("1부터 10까지 짝수합 : "+total);
+		
+		
+	}
+}
