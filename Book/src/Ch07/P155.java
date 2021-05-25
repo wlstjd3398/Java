d057bcf16fba7e4fb6f844bd14a467480ab26a45
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-05-25 13:19:00 +0900

    2021/05/25

---
+++ Book/src/Ch07/P155.java
@@ -0,0 +1,24 @@
+package Ch07;
+
+public class P155 {
+	public static void main(String[] args) {
+		Return obj = new Return();
+		
+		String name = obj.getName();
+		int age = obj.getAge();
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+	}
+}
+
+class Return {
+	String getName() {
+		return "ȫ�浿";
+	}
+	int getAge() {
+		return 30;
+	}
+}
