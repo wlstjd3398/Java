959d6e57ed63b96f728cae7f997762b2693f1f10
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-05-26 14:29:40 +0900

    2021/05/26

--- Book/src/Ch07/P160.java
+++ Book/src/Ch07/P160.java
@@ -2,6 +2,22 @@ package Ch07;
 
 public class P160 {
 	public static void main(String[] args) {
-		
+	MethodEx me = new MethodEx();
+	
+	me.one(); // 메서드 실행
+	}
+}
+
+class MethodEx {
+	void one() { // 1.
+		two();
+		System.out.println("one");
+	}
+	void two() { // 2.
+		three();
+		System.out.println("two");
+	}
+	void three() { // 3.
+		System.out.println("three");
 	}
 }
---
+++ Book/src/Ch07/P162.java
@@ -0,0 +1,8 @@
+package Ch07;
+
+public class P162 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
